5449f3029b692f7b6e73d9e90f0025551bf97437
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-07-23 23:52:21 +0300

    task 23-7

---
+++ 23-7 task/task - 23-7/task - 23-7/Controllers/ContactController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task_23_7.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(FormCollection form)
+        {
+            var name = form["Name"];
+            var phoneNumber = form["PhoneNumber"];
+            var gender = form["Gender"];
+            var colors = form["colors"];
+            var interests = form.GetValues("Interests");
+
+            TempData["Name"] = name;
+            TempData["PhoneNumber"] = phoneNumber;
+            TempData["Gender"] = gender;
+            TempData["colors"] = colors;
+            TempData["Interests"] = interests;
+
+            return RedirectToAction("Display");
+        }
+
+        public ActionResult Display()
+        {
+            ViewBag.Name = TempData["Name"];
+            ViewBag.PhoneNumber = TempData["PhoneNumber"];
+            ViewBag.Gender = TempData["Gender"];
+            ViewBag.colors = TempData["colors"];
+            ViewBag.Interests = TempData["Interests"];
+
+            return View();
+        }
+
+    }
+}
---
+++ 23-7 task/task - 23-7/task - 23-7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="task___23_7.MvcApplication" Language="C#" %>
